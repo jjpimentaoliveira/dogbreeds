b304d43631b0ce7675f5a861e93efa97f0702ff8
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-09-24 14:58:25 +0100

    Add .proj to repo

---
+++ DogBreeds/DogBreeds/DogBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsApp.swift
+//  DogBreeds
+//
+//  Created by José João Pimenta Oliveira on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
