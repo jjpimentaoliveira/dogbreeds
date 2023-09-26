b796ec7312e10df84e1e66d9525f5abc6c64ad73
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-09-26 16:45:36 +0100

    Call the new fetchBreedImage
    This ensures that all imageURL request are made async and that, in case of success, the dog breed is updated with the new information

--- DogBreeds/DogBreeds/ViewModels/DogBreedViewModel.swift
+++ DogBreeds/DogBreeds/ViewModels/DogBreedViewModel.swift
@@ -25,10 +25,45 @@ class DogBreedsViewModel: ObservableObject {
     func fetchDogBreeds() async {
         do {
             fetchState = .loading
-            let breeds = try await apiService.fetchDogBreeds()
+            var breeds = try await apiService.fetchDogBreeds()
+
+            try await fetchImageUrls(for: &breeds)
+
             fetchState = .fetched(breeds)
         } catch {
             fetchState = .error(error)
+        }
+    }
+
+    private func fetchImageUrls(for breeds: inout [DogBreed]) async throws {
+        try await withThrowingTaskGroup(of: DogBreed.self) { group in
+            for breed in breeds {
+                if let imageID = breed.referenceImageID {
+                    group.addTask {
+                        do {
+                            let imageURL = try await self.apiService.fetchBreedImage(for: imageID)
+                            let updatedBreed = DogBreed(existingBreed: breed, imageURL: imageURL)
+                            return updatedBreed
+                        } catch {
+                            print("Error: \(error.localizedDescription) while fetching breed: \(breed.id) imageURL")
+                            // Return the original breed
+                            return breed
+                        }
+                    }
+                } else {
+                    group.addTask {
+                        // No image ID, return the original breed
+                        return breed
+                    }
+                }
+            }
+
+            var updatedBreeds: [DogBreed] = []
+            for try await breed in group {
+                updatedBreeds.append(breed)
+            }
+
+            breeds = updatedBreeds
         }
     }
 }
