1a2901a6a90d4bc10b062758a86bb23111ae262f
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-09-26 15:12:40 +0100

    Add BreedListViewCell and BreedImage struct to proj

---
+++ DogBreeds/DogBreeds/Views/BreedListViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BreedListViewCell.swift
+//  DogBreeds
+//
+//  Created by José João Pimenta Oliveira on 26/09/2023.
+//
+
+import SwiftUI
+
+struct BreedListViewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BreedListViewCell()
+}
