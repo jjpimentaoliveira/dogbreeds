4fdffb3d13fb936a4f4dc8a0a14b90588c31172f
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-09-27 00:45:48 +0100

    adjust view folders and added DetailsView

---
+++ DogBreeds/DogBreeds/Views/DetailsView/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  DogBreeds
+//
+//  Created by José João Pimenta Oliveira on 27/09/2023.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
