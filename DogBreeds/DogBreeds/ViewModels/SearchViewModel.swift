1cd515bce68b23b8be9d2a8ed77b376177d9a82e
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-09-30 11:40:30 +0100

    Added new SearchViewModel
    This viewmodel should be responsible for handling the search and filtering logic

---
+++ DogBreeds/DogBreeds/ViewModels/SearchViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  SearchViewModel.swift
+//  DogBreeds
+//
+//  Created by José João Pimenta Oliveira on 30/09/2023.
+//
+
+import Foundation
+
+class SearchViewModel: ObservableObject {
+    @Published var filteredBreeds: [DogBreed] = []
+
+    func filterBreeds(
+        by query: String,
+        in breeds: [DogBreed]
+    ) {
+        if query.isEmpty {
+            filteredBreeds = breeds
+        } else {
+            print("Search query: \(query)")
+            filteredBreeds = breeds.filter { breed in
+                return breed.name?.range(of: query, options: .caseInsensitive) != nil
+            }
+        }
+    }
+}
