ad1aace17aedc3b5b0858b91029f0648788a90bb
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-09-30 11:42:58 +0100

    Created SearchViewCell
    This cell should be used on the search screen to present the filtered breeds

---
+++ DogBreeds/DogBreeds/Views/SearchView/SearchViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  SearchViewCell.swift
+//  DogBreeds
+//
+//  Created by José João Pimenta Oliveira on 30/09/2023.
+//
+
+import SwiftUI
+
+struct SearchViewCell: View {
+    let breed: DogBreed
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(breed.name ?? "")
+                .font(.headline)
+
+            if let breedGroup = breed.breedGroup, !breedGroup.isEmpty {
+                Text("Breed Group: \(breedGroup)")
+                    .font(.subheadline)
+            }
+
+            if let origin = breed.origin, !origin.isEmpty {
+                Text("Origin: \(origin)")
+                    .font(.subheadline)
+            }
+        }
+        .padding(8)
+    }
+}
+
+
+#Preview {
+    SearchViewCell(breed: DogBreed(
+        id: 1,
+        breedGroup: "Breed group",
+        name: "Test",
+        origin: "Origin",
+        temperament: "Temperament #1, temperament #2, temperament #3"
+    ))
+}
